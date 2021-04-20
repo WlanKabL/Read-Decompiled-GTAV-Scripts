c76df29fd9675d834016de959d70c8ed19009368
WlanKabl <55993320+PhilippStecher@users.noreply.github.com>
2021-04-20 10:41:26 +0200

    Better performance

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //Script decompiler: https://github.com/njames93/GTA-V-Script-Decompiler
-var fs = require('fs')
+var fs = require('fs');
 var Lazy = require('lazy');
 //onst readline = require('readline');
 
-var source = "freemode.ysc.c";//Your decompiled ysc.c
+var source = "freemode.c";//Your decompiled ysc.c
 var target = "remoteids.txt";
 
 var lineReader = require('readline').createInterface({
@@ -14,90 +14,73 @@ var TheRemoteIDHashes = [];
 var linecounter = 0;
 var foundids = 0;
 var begin = 0;//Line to start search
-var end = 0;//Line to end search
+var end = 1125129;//Line to end search
 
-function findid(currentline, ammountoftabs) {
-    spaces = ammountoftabs * 4;
-    var beginparse = false;
-    currentlinelenght = currentline.lenght
-    currentlinelenght = currentlinelenght - 1
-
-    currentremote = currentline.substr(spaces + 5, currentline.lenght)
-
-    var hashlenght = 0;
-    correcthash = "";
-
-    for (var x = 0; x <= currentremote.length - 1; x++) {
-        if (currentremote[x] != ":") {
-            hashlenght++;
-            correcthash += currentremote[x]
-        } else {
-            correcthash = correcthash
-        }
-    }
-
-    if (Number(correcthash) == NaN || Number(correcthash).toString() == "NaN") {
-        //console.log("Not a Number!")
-        return;
-    } else {
-        correcthash = correcthash.toString()
-        var adouble = false;
-        if (hashlenght >= 6 && hashlenght <= 15) {
-            for (var x = 0; x < foundids + 1; x++) {
-                if (correcthash == TheRemoteIDHashes[x]) {
-                    adouble = true;
-                }
-            }
-            if (!adouble) {
-                TheRemoteIDHashes.push(correcthash)
-                foundids++;
-            }
-
-        }
-    }
-
-
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
 
-var foundsearch = false
+var foundsearch = false;
 lineReader.on('line', function (line) {
 
     if (linecounter >= begin && linecounter <= end) {
-        if (line.substr(0, 5) == "case ") {
-            findid(line, 0)
-        } else if (line.substr(0, 9) == "    case ") {
-            findid(line, 1)
-        } else if (line.substr(0, 13) == "        case ") {
-            findid(line, 2)
-        } else if (line.substr(0, 17) == "            case ") {
-            findid(line, 3)
-        } else if (line.substr(0, 21) == "                case ") {
-            findid(line, 4)
-        }
-        if (!foundsearch) {
-            foundsearch = true
+        var ShortetLine = "";
+        if (line.startsWith("case")) {
+            ShortetLine = line.replace("case ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\tcase ")) {
+            ShortetLine = line.replace("\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\tcase ")) {
+            ShortetLine = line.replace("\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\t\tcase ")) {
+            ShortetLine = line.replace("\t\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\t\t\tcase ")) {
+            ShortetLine = line.replace("\t\t\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\t\t\t\tcase ")) {
+            ShortetLine = line.replace("\t\t\t\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\t\t\t\t\tcase ")) {
+            ShortetLine = line.replace("\t\t\t\t\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\t\t\t\t\t\tcase ")) {
+            ShortetLine = line.replace("\t\t\t\t\t\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
+        } else if (line.startsWith("\t\t\t\t\t\t\tcase ")) {
+            ShortetLine = line.replace("\t\t\t\t\t\t\t\tcase ", "");
+            ShortetLine = ShortetLine.replace(":", "");
         }
 
-    } else {
-        //console.log("NICHT im searchbereich")
+        if (ShortetLine.length > 5) {
+            if (isNumeric(ShortetLine)) {
+                TheRemoteIDHashes.push(ShortetLine);
+                foundids++;
+                if (!foundsearch) {
+                    foundsearch = true;
+                }
+            }
+        }
     }
     linecounter++;
-
-
 });
 
 forcpp = () => {
-    var theoutput = 'int remotes[] = {';
+    var theoutput = 'int remotes[' + foundids + '] = {';
     for (var x = 0; x < TheRemoteIDHashes.length; x++) {
         if (x == TheRemoteIDHashes.length - 1) {
-            theoutput = theoutput + TheRemoteIDHashes[x].toString()
+            theoutput = theoutput + TheRemoteIDHashes[x].toString();
         } else {
-            theoutput = theoutput + TheRemoteIDHashes[x].toString()
-            theoutput += ', '
+            theoutput = theoutput + TheRemoteIDHashes[x].toString();
+            theoutput += ', ';
         }
     }
-    theoutput += '};'
-    return theoutput
+    theoutput += '};';
+    return theoutput;
 }
 
 lineReader.on("close", function () {
@@ -111,10 +94,10 @@ lineReader.on("close", function () {
     }
 
     var forfile = forcpp();
-    fs.writeFile(target, forfile, function(err) {
-        if(err) {
+    fs.writeFile(target, forfile, function (err) {
+        if (err) {
             return console.log(err);
         }
         console.log("The file was saved!");
-    }); 
+    });
 })
